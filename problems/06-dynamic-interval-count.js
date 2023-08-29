efc1800ebfd579e380607a89caf0fe7c65f1ee79
Wei Cheng Chen <DrChenWeiCheng@gmail.com>
2023-08-28 21:49:10 -0700

    seis

--- problems/06-dynamic-interval-count.js
+++ problems/06-dynamic-interval-count.js
@@ -1,9 +1,15 @@
 /***********************************************************************
 Write a function, `dynamicIntervalCount`, that accepts a callback, a delay
-in milliseconds, and an optional amount as arguments. The function should 
-set an interval with the given callback and delay. If an amount argument 
+in milliseconds, and an optional amount as arguments. 
+
+The function should 
+set an interval with the given callback and delay. 
+
+If an amount argument 
 is passed, the interval should be cleared after the callback has been 
-called 'amount' number of times. If an amount argument is not passed,
+called 'amount' number of times. 
+
+If an amount argument is not passed,
 the interval should not be cleared and `dynamicIntervalCount` should instead
 return the Timeout object for the interval.
 
@@ -12,22 +18,33 @@ node with the examples below.
 
 Examples:
 
-dynamicIntervalCount(function() {
-    console.log('hi');
-}, 500, 3); // prints 'hi' at 500ms intervals a total of 3 times
 
-
-const timeoutObject = dynamicIntervalCount(function() {
-    console.log('hi');
-}, 500); // prints 'hi' at 500ms intervals indefinitely
-
-console.log(timeoutObject); // Timeout { ... }
 ***********************************************************************/
 
 function dynamicIntervalCount(cb, delay, amount) {
-  // Your code here 
+  if (!amount) {
+    return setInterval(cb, delay);
+  }
+  let initial = 0;
+  const interval1 = setInterval(() => {
+    cb();
+    initial++;
+    if (initial === amount) {
+      clearInterval(interval1)
+    }
+  }, delay);
+  
 }
+// dynamicIntervalCount(function() {
+//     console.log('hi');
+// }, 500, 3); // prints 'hi' at 500ms intervals a total of 3 times
 
+
+// const timeoutObject = dynamicIntervalCount(function() {
+//     console.log('hi');
+// }, 500); // prints 'hi' at 500ms intervals indefinitely
+
+// console.log(timeoutObject); // Timeout { ... }
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
   module.exports = dynamicIntervalCount;
